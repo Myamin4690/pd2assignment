715c909f290a209d2780330ce18d120bb83c2b39
Myamin4690 <studentyameenkhan@gmail.com>
2024-09-18 21:17:20 +0500

    Add files via upload

---
+++ pistol.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"        +--^---------- , -------- , ----- , --------^-,         "<<endl;
+cout<<"         | |||||||||    ,----------,       |           O        "<<endl;
+cout<<"         ,+--------------------------------^-----------|        "<<endl;
+cout<<"           ',---------,---------,--------------------'          "<<endl;
+cout<<"              / xxxxxx /,|       /,                             "<<endl;
+cout<<"             / xxxxxx /  |      /,                              "<<endl;
+cout<<"            / xxxxxx /,-------,                                 "<<endl;
+cout<<"           / xxxxxx /                                           "<<endl;
+cout<<"          / xxxxxx /                                            "<<endl;
+cout<<"         (--------(                                             "<<endl;
+cout<<"          ,------,                                              "<<endl;
+}
---
+++ testcolor.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 46");
+cout<<"this is my color text";
+
+
+}
